db3030ce712adb1e4c283d628610507c511953c8
Robson Barbosa <robson212.212@gmail.com>
2022-08-16 21:59:56 -0300

    State management

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {useState} from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 // import * as BooksAPI from './BooksAPI'
 import './App.css';
@@ -7,21 +7,14 @@ import Shelves from './components/Shelves';
 
 
 
-class BooksApp extends Component {
-  bookshelves = [
+const BooksApp = () => {
+  const bookshelves = [
     { shelf: 'Currently Reading' },
     { shelf: 'Want to Read' },
     { shelf: 'Have Read' },
   ];
-  state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
-    books: [
+
+    const startBooks = [
       {
         url: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
         title: "To Kill a Mockingbird",
@@ -35,61 +28,62 @@ class BooksApp extends Component {
         shelf: "wantToRead"
       }
     ]
-  };
 
-  render() {
-    return (
-      <div>
-        <div className="list-books">
-          <Header />
-          <div className="list-books-content">
-            <Shelves books={this.state.books}/>
-          </div>
-          <div className="open-search">
-            <Link to="Search">
-              <button>Add a Book</button>
-            </Link>
-            {}
-          </div>
-        </div>
-      </div>
-      
-    );
-  }
-}
+    const [showSearchPage, setShowSearchPage] = useState(false);
 
-class Search extends Component {
-  render() {
+    const [books, setBooks] = useState(startBooks)
+  
+    const updateBookShelf = (book, whereTo) => {
+      console.log(book)
+      console.log(whereTo)
+      const updatedBooks = books.map(b => {
+        if (b.id === book.id) {
+          book.shelf = whereTo;
+          return book;
+        }
+        return b;
+      })
+      setBooks(updatedBooks);
+    }
+
     return (
-      <div className="search-books">
-        <div className="search-books-bar">
-          <Link to="/" className="close-search">
-            Close
-          </Link>
-          {/* <button
-            className="close-search"
-            onClick={() => this.setState({ showSearchPage: false })}
-          >
-            Close
-          </button> */}
-          <div className="search-books-input-wrapper">
-            {/*
+      <div className="app">
+      {console.log(books)}
+      {showSearchPage ? (
+        <div className="search-books">
+          <div className="search-books-bar">
+            <button className="close-search" onClick={() => setShowSearchPage(false)}>Close</button>
+            <div className="search-books-input-wrapper">
+              {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-            <input type="text" placeholder="Search by title or author" />
+              <input type="text" placeholder="Search by title or author" />
+
+            </div>
+          </div>
+          <div className="search-books-results">
+            <ol className="books-grid"></ol>
           </div>
         </div>
-        <div className="search-books-results">
-          <ol className="books-grid" />
+      ) : (
+        <div className="list-books">
+          <Header />
+          <div className="list-books-content">
+            <Shelves books={books} updateBookShelf={updateBookShelf} />
+          </div>
+          <div className="open-search">
+            <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+          </div>
         </div>
-      </div>
+      )}
+    </div>
     );
   }
-}
+
+
 
 export default BooksApp;
--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
 import Book from './Book'
 
-const Shelf = ({books}) => {
+const Shelf = ({books, title}) => {
 
     return (
         <div className="bookshelf">
-              <h2 className="bookshelf-title">Currently Reading</h2>
+              <h2 className="bookshelf-title">{title}</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
                   {books.map((b) => 
