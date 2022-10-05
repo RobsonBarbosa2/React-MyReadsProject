7b391024bd3b7d0bd7a0793c4a691c1487125e06
Robson Barbosa <robson212.212@gmail.com>
2022-10-05 13:27:40 -0300

    Added search funcion

--- src/App.js
+++ src/App.js
@@ -1,50 +1,58 @@
-import React, {useState, useEffect} from 'react';
-import { Route, Routes, Link } from 'react-router-dom';
-import * as BooksAPI from './BooksAPI'
-import './App.css';
-import Header from './components/Header';
-import Shelves from './components/Shelves';
-
-
+import React, { useState, useEffect } from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
+import "./App.css";
+import Header from "./components/Header";
+import Shelves from "./components/Shelves";
+import Book from "./components/Book";
 
 const BooksApp = () => {
+  const [SearchBooks, setSearchBooks] = useState([]);
+  const [showSearchPage, setShowSearchPage] = useState(false);
+  const [query, setQuery] = useState([]);
+  const [books, setBooks] = useState([]);
 
-   
-      useEffect (() => {
-        BooksAPI.getAll() 
-        .then(data => 
-          {
-            console.log(data)
-            setBooks(data)
-          }
-        );
-      }, [])
-    
+  useEffect(() => {
+    BooksAPI.getAll().then((data) => {
+      setBooks(data);
+    });
+  }, []);
 
-    const [showSearchPage, setShowSearchPage] = useState(false);
-
-    const [books, setBooks] = useState([])
-  
-    const updateBookShelf = (book, whereTo) => {
-      console.log(book)
-      console.log(whereTo)
-      const updatedBooks = books.map(b => {
-        if (b.id === book.id) {
-          book.shelf = whereTo;
-          return book;
+  useEffect(() => {
+    if (query) {
+      BooksAPI.search(query).then((data) => {
+        console.log(data);
+        if (data.error) {
+          setSearchBooks([]);
+        } else {
+          setSearchBooks(data);
         }
-        return b;
-      })
-      setBooks(updatedBooks);
+      });
     }
+  }, [query]);
 
-    return (
-      <div className="app">
-      {console.log(books)}
+  const updateBookShelf = (book, whereTo) => {
+    const updatedBooks = books.map((b) => {
+      if (b.id === book.id) {
+        book.shelf = whereTo;
+        return book;
+      }
+      return b;
+    });
+    setBooks(updatedBooks);
+  };
+
+  return (
+    <div className="app">
       {showSearchPage ? (
         <div className="search-books">
           <div className="search-books-bar">
-            <button className="close-search" onClick={() => setShowSearchPage(false)}>Close</button>
+            <button
+              className="close-search"
+              onClick={() => setShowSearchPage(false)}
+            >
+              Close
+            </button>
             <div className="search-books-input-wrapper">
               {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -53,12 +61,22 @@ const BooksApp = () => {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-              <input type="text" placeholder="Search by title or author" />
-
+              <input
+                type="text"
+                placeholder="Search by title or author"
+                value={query}
+                onChange={(event) => setQuery(event.target.value)}
+              />
             </div>
           </div>
           <div className="search-books-results">
-            <ol className="books-grid"></ol>
+            <ol className="books-grid">
+              {SearchBooks.map((b) => (
+                <li key={b.id}>
+                  <Book books={b} changeShelf={updateBookShelf} />
+                </li>
+              ))}
+            </ol>
           </div>
         </div>
       ) : (
@@ -68,14 +86,12 @@ const BooksApp = () => {
             <Shelves books={books} updateBookShelf={updateBookShelf} />
           </div>
           <div className="open-search">
-            <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+            <button onClick={() => setShowSearchPage(true)}>Add a book</button>
           </div>
         </div>
       )}
     </div>
-    );
-  }
-
-
+  );
+};
 
 export default BooksApp;
