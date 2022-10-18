ae01f2b100508660637788d140d015aa09e29be8
Robson Barbosa <robson212.212@gmail.com>
2022-10-18 00:34:28 -0300

    Add AutoAnimate

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 import Book from "./Book";
 import PropTypes from "prop-types";
 
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+
 const Shelf = ({ books, title, updateBookShelf }) => {
+  const [parent] = useAutoAnimate(/* optional config */);
+
   return (
     <div className="bookshelf">
       <h2 className="bookshelf-title">{title}</h2>
       <div className="bookshelf-books">
-        <ol className="books-grid">
+        <ol ref={parent} className="books-grid">
           {books.map((b) => (
             <li key={b.id}>
               <Book books={b} changeShelf={updateBookShelf} />
